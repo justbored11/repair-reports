8b51159c2a4ae52a6acdff69650ff5b8dadcca51
Jesus Mendoza <jesus.doza@gmail.com>
2023-01-13 13:47:01 -0600

    server and app individual files

--- config/dbM.js
+++ config/dbM.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 
 //connect mongoose with mongodb connection string
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.connect_string, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`mongodb connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
+    try {
+        const conn = await mongoose.connect(process.env.connect_string, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`mongodb connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error("error a dbM.js", error);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
