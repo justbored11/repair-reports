d274b823974e2c6387479cd16032021503255989
Jesus Mendoza <101015821+jesusdoza@users.noreply.github.com>
2023-01-16 20:34:03 -0600

    Development (#133)
    * changed full menu flex
    
    * delete old unused stylesheets
    
    * mobile-menu partial renamed to be menu
    
    * Dashboard (#129) layout and unused views cleanup
    
    * delete old unused stylesheets
    
    * deleted unused ecm-logs view
    
    * mobile-menu partial renamed to be menu
    
    * area for repairs
    
    * server and app individual files
    
    * fixed display of dashboard repairs
    
    * fix user settings submenu position
    
    * add dashboard repair entries and groups display flex wrap
    
    * dashboard mobile query
    
    * ADD user repair to dashboard
    
    * ADD dashboard links to repairs
    
    * ADD dashboard links to repairs (#132)
    
    * change search bar css

--- controllers/dashboard/dashboard.js
+++ controllers/dashboard/dashboard.js
@@ -1,29 +1,21 @@
 ///dashboard controller
-const User = require("../models/User");
-const Repair = require("../models/Repair");
+const User = require("../../models/User");
+const Repair = require("../../models/Repair");
 const mongoose = require("mongoose");
+const { getUserRepairs } = require("../repair");
 
 async function getDashboard(req, res) {
-    // console.log(req.user)
-
-    const userRepairs = getUserRepairs(req.user);
+    const userRepairs = await getUserRepairs(req.user);
     const foundUser = await User.findById({ _id: req.user._id }).lean();
     const userGroups = foundUser.groups;
 
-    // console.log("foundUser", foundUser);
-    // console.log("usergroups", userGroups);
-
+    console.log(userRepairs);
     res.render("dashboard.ejs", {
         title: "Dashboard",
         user: req.user,
         groups: userGroups,
         repairs: userRepairs,
     });
-}
-
-async function getUserRepairs(user) {
-    const results = await Repair.find({ createdBy: user._id }).lean();
-    console.log("results ", results);
 }
 
 module.exports = { getDashboard };
